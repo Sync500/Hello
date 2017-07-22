d927a82a5200f6fc9f27ffc2134ffac465599ffb
Sync500 <syncmaster500@web.de>
2017-07-22 21:37:02 +0200

    change2

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello Scoty");
 		System.out.println("Hello Daniel2");
+		System.out.println("Hello Java");
 	}
 
 }
