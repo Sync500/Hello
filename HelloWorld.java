2518772cd92b1447fb54fc8fe1815c29e419949d
Sync500 <syncmaster500@web.de>
2017-07-22 21:54:45 +0200

    ..

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld Scoty");
+		System.out.println("HelloWorld Daniel2");
+		System.out.println("HelloWorld Java");
+	}
+
+}
