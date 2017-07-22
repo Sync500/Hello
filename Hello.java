e518531ae12ffc0115dcd8fdfde6bcb0018264c7
Sync500 <syncmaster500@web.de>
2017-07-22 21:52:34 +0200

    changeNameFromHelloToHelloWorld

--- Hello.java
+++
@@ -1,9 +0,0 @@
-
-public class Hello{
-	public static void main(String[] args){
-		System.out.println("Hello Scoty");
-		System.out.println("Hello Daniel2");
-		System.out.println("Hello Java");
-	}
-
-}
