d20ca9099c4cbd9bac39aa432154ad0930bce129
Sync500 <daniel.fay@uni-oldenburg.de>
2017-07-23 11:34:52 +0200

    change to 9

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("HelloWorld Scoty");
 		System.out.println("HelloWorld Daniel2");
-		System.out.println("HelloWorld Java8");
+		System.out.println("HelloWorld Java9");
 	}
 
 }
